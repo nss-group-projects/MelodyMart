276d197ab0cbdfe820f5cb34ddee8b2e444b9b7a
Josh Barton <joshdbarton@gmail.com>
2023-08-21 13:26:15 -0400

    initial push

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"  \  |      |           |         \  |            |   
+ |\/ |  _ \ |  _ \   _` | |   |  |\/ |  _` |  __| __| 
+ |   |  __/ | (   | (   | |   |  |   | (   | |    |   
+_|  _|\___|_|\___/ \__,_|\__, | _|  _|\__,_|_|   \__| 
+                         ____/                        ");
